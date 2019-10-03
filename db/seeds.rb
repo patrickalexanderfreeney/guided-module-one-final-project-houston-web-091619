469f98ebd068cd47fe8e1d5f357a22670f1c444c
Patrick Alexander <patrick_alexander@mail.com>
2019-10-03 14:01:10 -0500

    New changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@ Session.destroy_all
 
 
 m1 = Member.create(name: "Patrick", email: "pat@pat.com", password: "patpat")
+m2 = Member.create(name: "D-Money", email: "securethebag@git ")
 
 sp1 = Specialist.create(name: "Massage Master", specialty: "Massage Therapy")
 
